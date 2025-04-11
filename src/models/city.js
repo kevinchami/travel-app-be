f0f0295aee17b2176770a4101764c2f1b8efe5a0
Kevin Chami <kevinchamy@hotmail.com>
2025-04-11 15:43:08 +0300

    city update

--- src/models/city.js
+++ src/models/city.js
@@ -26,14 +26,14 @@ const accommodationSchema = new Schema({
 const citySchema = new Schema(
   {
     name: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, required: false },
     mapUrl: {type: String, required: false},
     available: { type: Boolean, required: false },
-    tours: [{ type: Schema.Types.ObjectId, ref: 'Tour' }],
-    temples: [{ type: Schema.Types.ObjectId, ref: 'Temple' }],
-    restaurants: [{ type: Schema.Types.ObjectId, ref: 'Restaurant' }],
+    tours: [{ type: Schema.Types.ObjectId, ref: 'Tour', required: false }],
+    temples: [{ type: Schema.Types.ObjectId, ref: 'Temple', required: false }],
+    restaurants: [{ type: Schema.Types.ObjectId, ref: 'Restaurant', required: false }],
     priority: { type: Number, required: false },
-    accommodations: [{ type: Schema.Types.ObjectId, ref: 'Accommodation' }], // Reference to the Accommodation model
+    accommodations: [{ type: Schema.Types.ObjectId, ref: 'Accommodation', required: false }], // Reference to the Accommodation model
     country: {
       type: Schema.Types.ObjectId,
       ref: 'Country',
