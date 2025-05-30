60c38ffa43a0d79cd1a816fe7f460d33335c44d8
Kevin Chami <kevinchamy@hotmail.com>
2025-05-30 08:46:53 +0300

    city region added

--- src/models/city.js
+++ src/models/city.js
@@ -27,18 +27,28 @@ const citySchema = new Schema(
   {
     name: { type: String, required: true },
     description: { type: String, required: false },
-    mapUrl: {type: String, required: false},
+    mapUrl: { type: String, required: false },
     available: { type: Boolean, required: false },
     tours: [{ type: Schema.Types.ObjectId, ref: 'Tour', required: false }],
     temples: [{ type: Schema.Types.ObjectId, ref: 'Temple', required: false }],
-    hide: {type: Boolean, required: false},
-    type: {type: String, required: false},
-    restaurants: [{ type: Schema.Types.ObjectId, ref: 'Restaurant', required: false }],
+    hide: { type: Boolean, required: false },
+    type: { type: String, required: false },
+    restaurants: [
+      { type: Schema.Types.ObjectId, ref: 'Restaurant', required: false },
+    ],
     priority: { type: Number, required: false },
-    accommodations: [{ type: Schema.Types.ObjectId, ref: 'Accommodation', required: false }], // Reference to the Accommodation model
+    accommodations: [
+      { type: Schema.Types.ObjectId, ref: 'Accommodation', required: false },
+    ], // Reference to the Accommodation model
     country: {
       type: Schema.Types.ObjectId,
       ref: 'Country',
+    },
+    region: {
+      latitude: { type: Number, required: false },
+      longitude: { type: Number, required: false },
+      latitudeDelta: { type: Number, required: false },
+      longitudeDelta: { type: Number, required: false },
     },
     coordinates: {
       latitude: { type: Number, required: false },
